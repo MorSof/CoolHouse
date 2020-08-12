f53b7deb3104b698f9e92af18f4659df765ecc82
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2020-08-13 00:01:52 +0300

    add doors with animations

---
+++ Assets/Scripts/TriggerDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour
+{
+
+    [SerializeField] GameObject door;
+         
+
+    private void OnTriggerEnter(Collider other)
+    {
+        door.GetComponent<Animator>().SetBool("doorOpen", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        door.GetComponent<Animator>().SetBool("doorOpen", false);
+
+    }
+}
