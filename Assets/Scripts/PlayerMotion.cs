707f2e8c4201b353a6117a83ce164fdcab5e17f1
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2020-09-18 12:29:08 +0300

    Add a maze and use Unity AI to make the people to catch the items

---
+++ Assets/Scripts/PlayerMotion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMotion : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private NavMeshAgent navMeshAgent = null;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+   
+
+    private void Start()
+    {
+        navMeshAgent.SetDestination(target.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        navMeshAgent.SetDestination(target.transform.position);
+    }
+
+    public void SetTarget(Transform target)
+    {
+        
+        navMeshAgent.SetDestination(target.position);
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    
+
+    [SerializeField] Transform[] pos = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("im here");
+
+        PlayerMotion playerMotion = other.GetComponent<PlayerMotion>();
+
+        if (playerMotion == null)
+        {
+            print("im null");
+            return;
+        }
+
+        
+        Vector3 newPos = pos[Random.Range(0, pos.Length - 1)].position;
+
+        while (newPos.x  == transform.position.x && newPos.z == transform.position.z)
+        {
+            newPos = pos[Random.Range(0, pos.Length - 1)].position;
+            print("im in while" + newPos);
+        }
+
+        print("my old pos is " + transform.position + "my new pos is " + newPos);
+        transform.position = newPos;
+      
+
+
+        playerMotion.SetTarget(transform);
+
+    }
+}
